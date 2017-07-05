48bea50d1a28becbd792959f1c38fa264b123eb2
Zak Holt <zholt@securiport.com>
2017-07-05 06:50:56 -0400

    remove error message that caused json stringify error

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const axios = require('axios')
 const { sendError } = require('micro')
 const assert = require('assert')
 
-const introspectToken = async ({ introspectionUrl, clientId, clientSecret, accessToken }) => {
-
+const introspectToken = async ({
+  introspectionUrl,
+  clientId,
+  clientSecret,
+  accessToken
+}) => {
   const { data: { active, sub: userId, scope } } = await axios({
     method: 'post',
     url: introspectionUrl,
@@ -24,28 +28,41 @@ const introspectToken = async ({ introspectionUrl, clientId, clientSecret, acces
   return { userId, scope }
 }
 
-module.exports = exports = (config) => (fn) => {
+module.exports = exports = config => fn => {
   const { introspectionUrl, clientId, clientSecret } = config
 
   if (!introspectionUrl || !clientId || !clientSecret) {
-    throw Error('Must provide config with introspectionUrl, clientId, and clientSecret properties')
+    throw Error(
+      'Must provide config with introspectionUrl, clientId, and clientSecret properties'
+    )
   }
 
   return async (req, res) => {
     const bearerToken = req.headers.authorization
 
     if (!bearerToken) {
-      return sendError(req, res, { statusCode: 401, message: 'missing Authorization header' })
+      return sendError(req, res, {
+        statusCode: 401,
+        message: 'missing Authorization header'
+      })
     }
 
     const accessToken = bearerToken.replace('Bearer ', '')
 
     try {
-      req.userData = await introspectToken({ clientId, clientSecret, introspectionUrl, accessToken })
+      req.userData = await introspectToken({
+        clientId,
+        clientSecret,
+        introspectionUrl,
+        accessToken
+      })
 
       return fn(req, res)
     } catch (error) {
-      return sendError(req, res, { statusCode: 403, status: 403, message: error })
+      return sendError(req, res, {
+        statusCode: 403,
+        status: 403
+      })
     }
   }
 }
